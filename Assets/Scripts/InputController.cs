58a7ffb49a5d0762aead832df33461f6e1dfd7b2
ravimahfunda <ravimahfunda@gmail.com>
2017-12-10 14:06:38 +0700

    Initial Commit

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InputController : MonoBehaviour {
+
+    public Animator playerAnimator;
+    public Animator enemyAnimator;
+    public Animator upgradeMenuAnimator;
+
+    public void Test(string msg) {
+        Debug.Log(msg);
+    }
+
+    public void Tap() {
+        //Set Animation
+        playerAnimator.SetTrigger("attack");
+        enemyAnimator.SetTrigger("attacked");
+
+        //And then what happend
+    }
+
+    public static string KEY_GOLD_CD = "GoldIsOnCooldown";
+
+    public void Gold(Button bt)
+    {
+        //Set Properties
+        bt.interactable = false;
+
+        //And then what happend
+        Test("Gold Clicked");
+        PlayerPrefs.SetInt(KEY_GOLD_CD,1);
+    }
+
+    public static string KEY_SKILL_CD = "SkillIsOnCooldown";
+    public void Skill(Button bt)
+    {
+        //Set Properties
+        bt.interactable = false;
+
+        //And then what happend
+        Test("Skill Clicked");
+        PlayerPrefs.SetInt(KEY_SKILL_CD, 1);
+    }
+
+    public static string KEY_REAPER_CD = "ReaperIsOnCooldown";
+    public void Reaper(Button bt)
+    {
+        //Set Properties
+        bt.interactable = false;
+
+        //And then what happend
+        Test("Reaper Clicked");
+        PlayerPrefs.SetInt(KEY_REAPER_CD, 1);
+    }
+
+    public void UpgradeMenu()
+    {
+        //Set Properties
+        upgradeMenuAnimator.SetBool("Show",!upgradeMenuAnimator.GetBool("Show"));
+
+        //And then what happend
+        Test("Upgrade Menu Clicked");
+    }
+}
---
+++ Assets/Scripts/StateManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StateManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    float goldCD = 5;
+    public Button goldButton;
+    public Slider goldSlider;
+    float timerA;
+
+    float skillCD = 5;
+    public Button skillButton;
+    public Slider skillSlider;
+    float timerB;
+
+    float reaperCD = 5;
+    public Button reaperButton;
+    public Slider reaperSlider;
+    float timerC;
+    // Update is called once per frame
+    void Update () {
+
+        //Gold is on cooldown
+        if (PlayerPrefs.GetInt("GoldIsOnCooldown",0) == 1) {
+            timerA += Time.deltaTime;
+
+            goldSlider.value += Time.deltaTime / goldCD;
+
+            if (timerA >= goldCD) {
+                goldButton.interactable = true;
+                goldSlider.value = 0;
+
+                Debug.Log("Cooldown done");
+                PlayerPrefs.SetInt("GoldIsOnCooldown", 0);
+                timerA = 0;
+            }
+        }
+
+        if (PlayerPrefs.GetInt("SkillIsOnCooldown", 0) == 1)
+        {
+            timerB += Time.deltaTime;
+
+            skillSlider.value += Time.deltaTime / skillCD;
+
+            if (timerB >= skillCD)
+            {
+                skillButton.interactable = true;
+                skillSlider.value = 0;
+
+                Debug.Log("Cooldown done");
+                PlayerPrefs.SetInt("SkillIsOnCooldown", 0);
+                timerB = 0;
+            }
+        }
+
+        if (PlayerPrefs.GetInt("ReaperIsOnCooldown", 0) == 1)
+        {
+            timerC += Time.deltaTime;
+
+            reaperSlider.value += Time.deltaTime / reaperCD;
+
+            if (timerC >= reaperCD)
+            {
+                reaperButton.interactable = true;
+                reaperSlider.value = 0;
+
+                Debug.Log("Cooldown done");
+                PlayerPrefs.SetInt("ReaperIsOnCooldown", 0);
+                timerC = 0;
+            }
+        }
+
+    }
+}
